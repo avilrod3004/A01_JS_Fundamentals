ce77612e243f4bd70b497e7d6a420f443bb3fd9a
avilrod3004 <avilrod3004@g.educaand.es>
2024-09-26 19:32:01 +0200

    ejercicio 4 ✓

---
+++ ejercicio_4/ejercicio_4.js
@@ -0,0 +1,6 @@
+let characters = "123";
+let numbers = 4;
+let result = characters + numbers;
+// como la primera variable es un string entiende el "+" como
+// una concatenación en vez de una suma
+console.log(result);
