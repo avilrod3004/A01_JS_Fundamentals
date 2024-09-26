ce91aae98d8959babb3dbbe29df832c26b821f59
avilrod3004 <avilrod3004@g.educaand.es>
2024-09-26 19:42:49 +0200

    ejercicio 5 ✓

---
+++ ejercicio_5/ejercicio_5.js
@@ -0,0 +1,10 @@
+let number1 = 35;
+let number2 = 12;
+
+console.log(number1 < number2);
+console.log(number1 > number2);
+console.log(number1 == number2);
+console.log(number1 != number2);
+
+console.log(number1 < number2 && number1 != number2);
+console.log(number1 != number2 || number1 > number2);
