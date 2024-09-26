69f02851df6cffcaa437ec96f1d208606c234bfb
avilrod3004 <avilrod3004@g.educaand.es>
2024-09-26 19:24:17 +0200

    ejercicio 3 ✓

---
+++ ejercicio_3/ejercicio_3.js
@@ -0,0 +1,10 @@
+let number1 = 30;
+let number2 = 5;
+
+console.log(`Suma: ${number1 + number2}`);
+console.log(`Resta: ${number1 - number2}`);
+console.log(`Multiplicación: ${number1 * number2}`);
+console.log(`División: ${number1 / number2}`);
+
+console.log(number1++)
+console.log(number2--)
