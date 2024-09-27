9015c8a38e6701fc729df649cbb4d37125ef2994
avilrod3004 <avilrod3004@g.educaand.es>
2024-09-27 10:24:44 +0200

    ejercicio 8 ✓

---
+++ ejercicio_8/ejercicio_8.js
@@ -0,0 +1,16 @@
+let age = prompt("¿Cuál es tu edad?");
+
+
+if (!isNaN(age)) {
+    if (age < 18) {
+        alert("Es menor de edad")
+    } else {
+        if (age >= 18 && age <= 25) {
+            alert("Tiene entre 18 y 25 años")
+        } else {
+            alert("Es mayor de edad")
+        }
+    }
+} else {
+    alert("ERROR - Introduce un número")
+}
