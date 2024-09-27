cb737a9657fdb7f213350f750e145a2b0d3b6e74
avilrod3004 <avilrod3004@g.educaand.es>
2024-09-27 10:31:49 +0200

    ejercicio 6 ✓

--- ejercicio_6/ejercicio_6.js
+++ ejercicio_6/ejercicio_6.js
@@ -1,6 +1,7 @@
 let number = prompt("Introduce un número:")
 
 if (!isNaN(number)) {
+    number = Number(number);
     number > 0 ? alert("Es positivo") : number < 0 ? alert("Es negativo") : alert("Es cero")
 } else {
     alert("ERROR - Introduce un número")
